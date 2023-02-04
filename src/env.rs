540421f81ea1fccf609d9758b0d477c8ed2dec02
Stéphane Travostino <steph@combo.cc>
2023-02-04 09:40:12 +0000

    Add env tests

--- src/env.rs
+++ src/env.rs
@@ -21,3 +21,17 @@ impl Env {
         self.slots.insert(name.to_owned(), value);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_set() {
+        let mut env = Env::new();
+        assert!(env.get("foo").is_none());
+
+        env.set("foo", Expr::Symbol("bar".to_owned()));
+        assert_eq!(env.get("foo"), Some(&Expr::Symbol("bar".to_owned())));
+    }
+}
